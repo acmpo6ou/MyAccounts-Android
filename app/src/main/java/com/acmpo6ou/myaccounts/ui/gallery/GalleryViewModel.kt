25b2eeec1d2f98d90c10ea098c93166e42b31efa
Acmpo6ou <acmpo6ou@gmail.com>
2020-10-20 18:24:21 +0300

    Initialize MyAccounts project

---
+++ app/src/main/java/com/acmpo6ou/myaccounts/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.acmpo6ou.myaccounts.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
