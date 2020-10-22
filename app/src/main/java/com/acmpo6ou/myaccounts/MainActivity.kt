d8238370d8cfb30c99c521f191a17a2618b38c26
Acmpo6ou <acmpo6ou@gmail.com>
2020-10-22 20:42:21 +0300

    Delete code of useless menu, fix code of app menu

--- app/src/main/java/com/acmpo6ou/myaccounts/MainActivity.kt
+++ app/src/main/java/com/acmpo6ou/myaccounts/MainActivity.kt
@@ -29,15 +29,8 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-//        appBarConfiguration = AppBarConfiguration(setOf(
-//                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
     }
 }
