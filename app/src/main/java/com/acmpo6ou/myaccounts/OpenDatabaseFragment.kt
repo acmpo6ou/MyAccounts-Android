32bc868429cada4b620cd2d55339b551be2ae7e0
Acmpo6ou <acmpo6ou@gmail.com>
2020-10-24 10:07:22 +0300

    Create OpenDatabaseFragment with password field and open button

---
+++ app/src/main/java/com/acmpo6ou/myaccounts/OpenDatabaseFragment.kt
@@ -0,0 +1,31 @@
+package com.acmpo6ou.myaccounts
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class OpenDatabaseFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = OpenDatabaseFragment()
+    }
+
+    private lateinit var viewModel: OpenDatabaseViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.open_database_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(OpenDatabaseViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/acmpo6ou/myaccounts/OpenDatabaseViewModel.kt
@@ -0,0 +1,7 @@
+package com.acmpo6ou.myaccounts
+
+import androidx.lifecycle.ViewModel
+
+class OpenDatabaseViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
