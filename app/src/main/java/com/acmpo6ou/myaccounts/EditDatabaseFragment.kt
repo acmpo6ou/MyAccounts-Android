90719f5ce2951d5973471d1eafcaf46208f90934
Acmpo6ou <acmpo6ou@gmail.com>
2020-10-25 08:58:27 +0200

    Create EditDatabaseFragment

---
+++ app/src/main/java/com/acmpo6ou/myaccounts/EditDatabaseFragment.kt
@@ -0,0 +1,29 @@
+package com.acmpo6ou.myaccounts
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class EditDatabaseFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = EditDatabaseFragment()
+    }
+
+    private lateinit var viewModel: EditDatabaseViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.edit_database_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(EditDatabaseViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/acmpo6ou/myaccounts/EditDatabaseViewModel.kt
@@ -0,0 +1,7 @@
+package com.acmpo6ou.myaccounts
+
+import androidx.lifecycle.ViewModel
+
+class EditDatabaseViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
