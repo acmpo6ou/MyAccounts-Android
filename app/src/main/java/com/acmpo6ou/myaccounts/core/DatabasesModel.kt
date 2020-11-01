0518f7bf713ac15279464da7bcbdf744817e6118
Acmpo6ou <acmpo6ou@gmail.com>
2020-11-01 17:40:27 +0200

    Create package `core` which will contain all core functionality
    Create DatabasesModel.kt file which will contain classes related to DatabasesModel.
    Create Database data class and documented it

---
+++ app/src/main/java/com/acmpo6ou/myaccounts/core/DatabasesModel.kt
@@ -0,0 +1,20 @@
+package com.acmpo6ou.myaccounts.core
+
+/** This module contains classes related to DatabasesModel */
+
+/**
+ * Class that represents database of Accounts.
+ *
+ * @param[name] name of the database.
+ * @param[password] password of the database.
+ * @param[data] map of account names to corresponding Account instances.
+ * @property isOpen dynamically returns whether database is open or not, the database is
+ * considered open when [password] is not null.
+ */
+data class Database(val name: String,
+                    val password: String? = null,
+                    val data: Map<String, Int>? = null){
+    var isOpen: Boolean = false
+        get() = password != null
+        private set
+}
