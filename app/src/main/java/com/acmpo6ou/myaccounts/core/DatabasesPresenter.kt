bb470bdbee4f12708f5a28d6a9a11a82d11a480d
Acmpo6ou <acmpo6ou@gmail.com>
2020-11-18 16:49:19 +0200

    Create stub DatabasesPresenter

---
+++ app/src/main/java/com/acmpo6ou/myaccounts/core/DatabasesPresenter.kt
@@ -0,0 +1,9 @@
+package com.acmpo6ou.myaccounts.core
+
+import com.acmpo6ou.myaccounts.ui.DatabaseFragment
+
+class DatabasesPresenter: DatabasesPresenterInter {
+    override fun exportDatabase(location: String) {
+
+    }
+}
