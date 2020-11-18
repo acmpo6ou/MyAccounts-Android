cf1e3151cd9691ebf42261b9731ef15f7ddaacf9
Acmpo6ou <acmpo6ou@gmail.com>
2020-11-18 20:46:01 +0200

    Create a test for DatabaseFragment.showSuccess()

--- app/src/test/java/com/acmpo6ou/myaccounts/DatabaseFragmentInstrumentation.kt
+++ app/src/test/java/com/acmpo6ou/myaccounts/DatabaseFragmentInstrumentation.kt
@@ -20,13 +20,19 @@
 package com.acmpo6ou.myaccounts
 
 import android.content.Intent
+import android.os.Looper.getMainLooper
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.testing.*
 import androidx.navigation.Navigation
 import androidx.navigation.testing.TestNavHostController
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.platform.app.InstrumentationRegistry
 import com.acmpo6ou.myaccounts.core.*
 import com.acmpo6ou.myaccounts.ui.DatabaseFragment
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.SnackbarContentLayout
 import com.nhaarman.mockitokotlin2.mock
 import org.junit.Assert.*
 import org.junit.*
@@ -35,10 +41,44 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
 import org.robolectric.Shadows.shadowOf
 
+/**
+ * @return a TextView if a snackbar is shown anywhere in the view hierarchy.
+ *
+ * NOTE: calling Snackbar.make() does not create a snackbar. Only calling #show() will create it.
+ *
+ * If the textView is not-null you can check its text.
+ */
+fun View.findSnackbarTextView(): TextView? {
+    val possibleSnackbarContentLayout = findSnackbarLayout()?.getChildAt(0) as? SnackbarContentLayout
+    return possibleSnackbarContentLayout
+            ?.getChildAt(0) as? TextView
+}
+
+private fun View.findSnackbarLayout(): Snackbar.SnackbarLayout? {
+    when (this) {
+        is Snackbar.SnackbarLayout -> return this
+        !is ViewGroup -> return null
+    }
+    // otherwise traverse the children
+
+    // the compiler needs an explicit assert that `this` is an instance of ViewGroup
+    this as ViewGroup
+
+    (0 until childCount).forEach { i ->
+        val possibleSnackbarLayout = getChildAt(i).findSnackbarLayout()
+        if (possibleSnackbarLayout != null) return possibleSnackbarLayout
+    }
+    return null
+}
+
 @RunWith(RobolectricTestRunner::class)
 class DatabaseFragmentInstrumentation {
     lateinit var navController: TestNavHostController
     lateinit var databaseScenario: FragmentScenario<DatabaseFragment>
+
+    // get string resources
+    val context = InstrumentationRegistry.getInstrumentation().targetContext
+    val successMessage = context.resources.getString(R.string.success_message)
 
     @Before
     fun setUp(){
@@ -85,5 +125,27 @@ class DatabaseFragmentInstrumentation {
 
         val actual: Intent = shadowOf(RuntimeEnvironment.application).nextStartedActivity
         assertEquals(expectedIntent, actual)
+    }
+
+    @Test
+    fun `showSuccess should display snackbar`(){
+        databaseScenario.onFragment {
+            // call showSuccess and get the snackbar
+            it.showSuccess()
+            shadowOf(getMainLooper()).idle()
+            val snackbar: TextView? = it.view?.findSnackbarTextView()
+
+            // check that snackbar was displayed
+            assertTrue(
+        "No snackbar is displayed when call to DatabaseFragment.showSuccess is made!",
+        snackbar != null)
+
+            // check the snackbar's message
+            assertEquals(
+                    "showSuccess snackbar has incorrect message!",
+                    successMessage,
+                    snackbar?.text
+            )
+        }
     }
 }
