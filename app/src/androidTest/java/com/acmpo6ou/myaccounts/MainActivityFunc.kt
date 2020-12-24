bf04b8b1aacf068eeee197afb961b638c4aa17d2
Acmpo6ou <acmpo6ou@gmail.com>
2020-12-24 10:02:22 +0200

    Start writing espresso test for navigation drawer closing on back button press

---
+++ app/src/androidTest/java/com/acmpo6ou/myaccounts/MainActivityFunc.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020. Kolvakh Bohdan
+ * This file is part of MyAccounts.
+ *
+ * MyAccounts is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyAccounts is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.acmpo6ou.myaccounts
+
+import android.view.Gravity
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.DrawerActions
+import androidx.test.espresso.contrib.DrawerMatchers.isClosed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityFunc {
+    lateinit var mainScenario: ActivityScenario<MainActivity>
+
+    @Before
+    fun setup() {
+        mainScenario = ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @Test
+    fun back_button_should_close_nav_drawer(){
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT))) // Drawer should be closed.
+                .perform(DrawerActions.open())
+        Thread.sleep(5000)
+    }
+}
