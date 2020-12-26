a69b4320026dcf431208c5c3c6a0c1893204d37a
Acmpo6ou <acmpo6ou@gmail.com>
2020-12-26 16:17:35 +0200

    Create file for unit tests of OpenDatabaseViewModel

---
+++ app/src/test/java/com/acmpo6ou/myaccounts/open_database/OpenDatabaseViewModelTests.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2020. Kolvakh Bohdan
+ * This file is part of MyAccounts.
+ *
+ * MyAccounts is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyAccounts is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.acmpo6ou.myaccounts.open_database
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Rule
+
+class OpenDatabaseViewModelTests {
+    @get:Rule
+    val taskExecutorRule = InstantTaskExecutorRule()
+}
