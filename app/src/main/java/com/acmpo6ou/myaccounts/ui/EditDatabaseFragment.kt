ca22fa8c6395253a538c26d1098cff6a0f41ecf9
Acmpo6ou <acmpo6ou@gmail.com>
2021-01-25 19:24:06 +0200

    EditDatabaseFragment now inherits from CreateEditFragment

--- app/src/main/java/com/acmpo6ou/myaccounts/ui/EditDatabaseFragment.kt
+++ app/src/main/java/com/acmpo6ou/myaccounts/ui/EditDatabaseFragment.kt
@@ -19,31 +19,21 @@
 
 package com.acmpo6ou.myaccounts.ui
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.acmpo6ou.myaccounts.R
+import androidx.lifecycle.ViewModelProvider
+import com.acmpo6ou.myaccounts.core.CreateEditFragment
 
-class EditDatabaseFragment : Fragment() {
-
+class EditDatabaseFragment : CreateEditFragment() {
     companion object {
         fun newInstance() = EditDatabaseFragment()
     }
-
-    private lateinit var viewModel: EditDatabaseViewModel
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.create_edit_database_fragment, container, false)
-    }
+    override lateinit var viewModel: EditDatabaseViewModel
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(EditDatabaseViewModel::class.java)
-        // TODO: Use the ViewModel
+        initModel()
+        initForm()
     }
 
 }
