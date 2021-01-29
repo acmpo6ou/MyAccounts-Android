70d0f873af5faa2f624d7fbfaf5a432545886dda
Acmpo6ou <acmpo6ou@gmail.com>
2021-01-29 10:36:12 +0200

    Create adapter for AboutFragment

---
+++ app/src/main/java/com/acmpo6ou/myaccounts/ui/about/AboutAdapter.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2020-2021. Bohdan Kolvakh
+ * This file is part of MyAccounts.
+ *
+ * MyAccounts is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyAccounts is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.acmpo6ou.myaccounts.ui.about
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class AboutAdapter(fm: FragmentManager) :
+        FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    override fun getCount() = 3
+
+    override fun getItem(position: Int): Fragment {
+        return when(position){
+            0 -> AboutTab()
+            1 -> LicenseTab()
+            else -> CreditsTab()
+        }
+    }
+}
