79e1e5c64771ce010a47db7ab2eb4fde4bd18b15
Acmpo6ou <acmpo6ou@gmail.com>
2021-01-29 10:22:03 +0200

    AboutFragment will set versionLabel text to current app version

--- app/src/main/java/com/acmpo6ou/myaccounts/ui/AboutFragment.kt
+++ app/src/main/java/com/acmpo6ou/myaccounts/ui/AboutFragment.kt
@@ -23,7 +23,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.acmpo6ou.myaccounts.BuildConfig
 import com.acmpo6ou.myaccounts.R
 
 class AboutFragment : Fragment() {
@@ -31,5 +33,14 @@ class AboutFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // set app version
+        val versionLabel = view.findViewById<TextView>(R.id.versionLabel)
+        val versionStr =
+                String.format(requireActivity().resources.getString(R.string.version),
+                              BuildConfig.VERSION_NAME)
+        versionLabel.text = versionStr
     }
 }
