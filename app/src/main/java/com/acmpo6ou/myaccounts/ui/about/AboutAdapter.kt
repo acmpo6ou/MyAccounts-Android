fdac90ce85f690e5b1f30b84268302cb8df0c3a8
Acmpo6ou <acmpo6ou@gmail.com>
2021-01-29 13:48:28 +0200

    Update adapter for ViewPager2

--- app/src/main/java/com/acmpo6ou/myaccounts/ui/about/AboutAdapter.kt
+++ app/src/main/java/com/acmpo6ou/myaccounts/ui/about/AboutAdapter.kt
@@ -20,14 +20,12 @@
 package com.acmpo6ou.myaccounts.ui.about
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class AboutAdapter(fm: FragmentManager) :
-        FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
-    override fun getCount() = 3
+class AboutAdapter(fragment: Fragment) : FragmentStateAdapter(fragment){
+    override fun getItemCount() = 3
 
-    override fun getItem(position: Int): Fragment {
+    override fun createFragment(position: Int): Fragment {
         return when(position){
             0 -> AboutTab()
             1 -> LicenseTab()
