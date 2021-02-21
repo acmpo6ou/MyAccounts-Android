a1b57a787ad38494cb484c13c744f577033b6f20
Acmpo6ou <acmpo6ou@gmail.com>
2021-02-21 16:54:52 +0200

    Write test for setAccount

---
+++ app/src/test/java/com/acmpo6ou/myaccounts/display_account/DisplayAccountInst.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2020-2021. Bohdan Kolvakh
+ * This file is part of MyAccounts.
+ *
+ * MyAccounts is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyAccounts is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.acmpo6ou.myaccounts.display_account
+
+import android.content.Context
+import android.os.Build
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.platform.app.InstrumentationRegistry
+import com.acmpo6ou.myaccounts.R
+import com.acmpo6ou.myaccounts.account
+import com.acmpo6ou.myaccounts.ui.account.DisplayAccountFragment
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.LooperMode
+
+@RunWith(RobolectricTestRunner::class)
+@LooperMode(LooperMode.Mode.PAUSED)
+@Config(sdk = [Build.VERSION_CODES.O_MR1])
+class DisplayAccountInst {
+    lateinit var scenario: FragmentScenario<DisplayAccountFragment>
+    val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
+
+    val usernameStr = context.resources.getString(R.string.username_)
+    val emailStr = context.resources.getString(R.string.e_mail_)
+    val passwordStr = context.resources.getString(R.string.password_)
+    val dateOfBirthStr = context.resources.getString(R.string.date_of_birth_)
+    val commentStr = context.resources.getString(R.string.comment_)
+
+    @Before
+    fun setUp() {
+        scenario = launchFragmentInContainer(themeResId = R.style.Theme_MyAccounts_NoActionBar)
+    }
+
+    @Test
+    fun `setAccount should fill all text views with data`(){
+        scenario.onFragment{
+            it.setAccount(account)
+
+            assertEquals("$usernameStr ${account.name}", it.b.accountUsername.text)
+            assertEquals("$emailStr ${account.email}", it.b.accountEmail.text)
+            assertEquals("$passwordStr ${account.password}", it.b.accountPassword.text)
+            assertEquals("$dateOfBirthStr ${account.date}", it.b.birthDate.text)
+            assertEquals("$commentStr\n${account.comment}", it.b.accountComment.text)
+        }
+    }
+}
