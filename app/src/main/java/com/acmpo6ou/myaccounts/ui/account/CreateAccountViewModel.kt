15c9de8ece0bf2a40c4ae0ee890133879dad1036
Acmpo6ou <acmpo6ou@gmail.com>
2021-02-27 13:09:42 +0200

    Implement CreateAccountViewModel

--- app/src/main/java/com/acmpo6ou/myaccounts/ui/account/CreateAccountViewModel.kt
+++ app/src/main/java/com/acmpo6ou/myaccounts/ui/account/CreateAccountViewModel.kt
@@ -19,8 +19,35 @@
 
 package com.acmpo6ou.myaccounts.ui.account
 
-import androidx.lifecycle.ViewModel
+import com.acmpo6ou.myaccounts.core.MyApp
+import com.acmpo6ou.myaccounts.core.superclass.CreateEditViewModel
+import com.acmpo6ou.myaccounts.database.Account
+import com.acmpo6ou.myaccounts.database.DbMap
 
-class CreateAccountViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class CreateAccountViewModel : CreateEditViewModel() {
+    override lateinit var app: MyApp
+    var databaseIndex = 999
+
+    val accounts: DbMap get() = app.databases[databaseIndex].data
+    override val itemNames get() = accounts.values.toList().map { it.accountName }
+
+    /**
+     * Initializes model with needed resources.
+     * @param[app] application instance used to get database that contains accounts map.
+     * @param[databaseIndex] index of database that contains accounts map.
+     */
+    fun initialize(app: MyApp, databaseIndex: Int){
+        this.app = app
+        this.databaseIndex = databaseIndex
+    }
+
+    /**
+     * Called when user presses apply button.
+     * Creates new account using information provided.
+     */
+    fun applyPressed(accountName: String, username: String, email: String,
+                     password: String, date: String, comment: String){
+        accounts[accountName] = Account(accountName, username, email, password, date, comment)
+        finished = true // notify about creation
+    }
 }
