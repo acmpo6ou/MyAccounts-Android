c870dfc453e94d1f986c9bd7a4eb044adba8e3e5
Acmpo6ou <acmpo6ou@gmail.com>
2021-03-07 14:36:43 +0200

    Remove UpdatesFragment because we have UpdatesActivity

--- app/src/main/java/com/acmpo6ou/myaccounts/UpdatesActivity.kt
+++ app/src/main/java/com/acmpo6ou/myaccounts/UpdatesActivity.kt
@@ -21,18 +21,11 @@ package com.acmpo6ou.myaccounts
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.acmpo6ou.myaccounts.ui.UpdatesFragment
 
 class UpdatesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.updates_activity)
-
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, UpdatesFragment.newInstance())
-                .commitNow()
-        }
     }
 }
