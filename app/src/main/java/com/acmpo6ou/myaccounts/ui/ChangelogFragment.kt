9960027d007a2160165520818777322b6f7420cf
Acmpo6ou <acmpo6ou@gmail.com>
2021-03-14 10:38:08 +0200

    Write code for ChangelogFragment to load changelog from raw/changelog

--- app/src/main/java/com/acmpo6ou/myaccounts/ui/ChangelogFragment.kt
+++ app/src/main/java/com/acmpo6ou/myaccounts/ui/ChangelogFragment.kt
@@ -20,60 +20,32 @@
 package com.acmpo6ou.myaccounts.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.text.HtmlCompat
+import androidx.core.text.HtmlCompat.fromHtml
+import androidx.fragment.app.Fragment
 import com.acmpo6ou.myaccounts.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ChangelogFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ChangelogFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_changelog, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ChangelogFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ChangelogFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val changelogLabel = view.findViewById<TextView>(R.id.changelogLabel)
+        val resources = requireActivity().resources
+
+        // load changelog from `raw/changelog`
+        val changelog =
+                resources.openRawResource(
+                resources.getIdentifier("changelog", "raw",
+                        requireActivity().packageName))
+        changelog.bufferedReader().use {
+            changelogLabel.text = fromHtml(it.readText(), HtmlCompat.FROM_HTML_MODE_COMPACT)
+        }
     }
 }
