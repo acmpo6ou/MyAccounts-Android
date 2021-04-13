26b92d22b3fe2dae5830d4dce7976f41e30712f4
Acmpo6ou <acmpo6ou@gmail.com>
2021-04-13 13:20:48 +0300

    Adapt DatabasesFragmentInst to Hilt dependency injection

--- app/src/test/java/com/acmpo6ou/myaccounts/databases_list/DatabasesFragmentInst.kt
+++ app/src/test/java/com/acmpo6ou/myaccounts/databases_list/DatabasesFragmentInst.kt
@@ -21,42 +21,58 @@ package com.acmpo6ou.myaccounts.databases_list
 
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
-import androidx.fragment.app.testing.FragmentScenario
-import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation.setViewNavController
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.platform.app.InstrumentationRegistry
 import com.acmpo6ou.myaccounts.MyApp
 import com.acmpo6ou.myaccounts.R
-import com.acmpo6ou.myaccounts.database.databases_list.Database
-import com.acmpo6ou.myaccounts.database.databases_list.DatabasesFragment
+import com.acmpo6ou.myaccounts.core.AppModule
+import com.acmpo6ou.myaccounts.database.databases_list.*
 import com.acmpo6ou.myaccounts.database.databases_list.DatabasesFragmentDirections.actionEditDatabase
 import com.acmpo6ou.myaccounts.database.databases_list.DatabasesFragmentDirections.actionOpenDatabase
-import com.acmpo6ou.myaccounts.database.databases_list.DatabasesPresenter
+import com.acmpo6ou.myaccounts.launchFragmentInHiltContainer
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
+import dagger.hilt.android.scopes.FragmentScoped
+import dagger.hilt.android.testing.BindValue
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
 import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
 import org.robolectric.Shadows.shadowOf
-import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
 import org.robolectric.shadows.ShadowAlertDialog
+import javax.inject.Singleton
 
+@HiltAndroidTest
 @RunWith(RobolectricTestRunner::class)
+@UninstallModules(AppModule::class, DatabasesBindings::class)
 @LooperMode(LooperMode.Mode.PAUSED)
-@Config(sdk = [Build.VERSION_CODES.O_MR1])
 class DatabasesFragmentInst {
-    lateinit var scenario: FragmentScenario<DatabasesFragment>
+    @get:Rule var hiltAndroidRule = HiltAndroidRule(this)
+
+    lateinit var fragment: DatabasesFragment
     private lateinit var navController: NavController
+
+    @BindValue
+    @JvmField
+    @Singleton
+    val app = MyApp()
+
+    @BindValue
+    @JvmField
+    @FragmentScoped
+    val presenter: DatabasesPresenterI = mock()
 
     // get string resources
     private val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
@@ -66,14 +82,12 @@ class DatabasesFragmentInst {
 
     @Before
     fun setUp() {
-        scenario = launchFragmentInContainer(themeResId = R.style.Theme_MyAccounts_NoActionBar)
-
-        val app = MyApp()
         app.databases = mutableListOf(Database("main"))
-        scenario.onFragment { it.app = app }
+        hiltAndroidRule.inject()
+        fragment = launchFragmentInHiltContainer()
     }
 
-    private fun mockNavController(fragment: DatabasesFragment) {
+    private fun mockNavController() {
         navController = mock()
         setViewNavController(fragment.requireView(), navController)
     }
@@ -85,9 +99,7 @@ class DatabasesFragmentInst {
         val expectedType = "application/x-tar"
         val expectedTitle = "main.tar"
 
-        scenario.onFragment {
-            it.exportDialog(0)
-        }
+        fragment.exportDialog(0)
 
         // check all intent properties
         val intent: Intent = shadowOf(RuntimeEnvironment.application).nextStartedActivity
@@ -109,91 +121,64 @@ class DatabasesFragmentInst {
             expectedTitle, intent.getStringExtra(Intent.EXTRA_TITLE)
         )
     }
-
     @Test
     fun `navigateToEdit should pass appropriate database index`() {
-        scenario.onFragment {
-            mockNavController(it)
-            it.navigateToEdit(0)
+        mockNavController()
+        fragment.navigateToEdit(0)
 
-            val expectedAction = actionEditDatabase(0)
-            verify(navController).navigate(expectedAction)
-        }
+        val expectedAction = actionEditDatabase(0)
+        verify(navController).navigate(expectedAction)
     }
 
     @Test
     fun `navigateToOpen should pass appropriate database index`() {
-        scenario.onFragment {
-            mockNavController(it)
-            it.navigateToOpen(0)
+        mockNavController()
+        fragment.navigateToOpen(0)
 
-            val expectedAction = actionOpenDatabase(0)
-            verify(navController).navigate(expectedAction)
-        }
+        val expectedAction = actionOpenDatabase(0)
+        verify(navController).navigate(expectedAction)
     }
 
     @Test
     fun `confirmDelete should create dialog with appropriate message and title`() {
-        scenario.onFragment {
-            it.confirmDelete(0)
-        }
+        fragment.confirmDelete(0)
 
         val dialog = ShadowAlertDialog.getLatestDialog() as AlertDialog
         val title = dialog.findViewById<TextView>(R.id.alertTitle)
         val message = dialog.findViewById<TextView>(android.R.id.message)
 
-        assertEquals(
-            "confirmDelete created dialog with incorrect title!",
-            warningTitle, title?.text
-        )
-        assertEquals(
-            "confirmDelete created dialog with incorrect message!",
-            String.format(confirmDeleteMsg, "main"), message?.text
-        )
+        assertEquals(warningTitle, title?.text)
+        assertEquals(String.format(confirmDeleteMsg, "main"), message?.text)
     }
 
     @Test
     fun `confirmClose should create dialog with appropriate message and title`() {
-        scenario.onFragment {
-            it.confirmClose(0)
-        }
+        fragment.confirmClose(0)
 
         val dialog = ShadowAlertDialog.getLatestDialog() as AlertDialog
         val title = dialog.findViewById<TextView>(R.id.alertTitle)
         val message = dialog.findViewById<TextView>(android.R.id.message)
 
-        assertEquals(
-            "confirmClose created dialog with incorrect title!",
-            warningTitle, title?.text
-        )
-        assertEquals(
-            "confirmClose created dialog with incorrect message!",
-            String.format(confirmCloseMsg, "main"), message?.text
-        )
+        assertEquals(warningTitle, title?.text)
+        assertEquals(String.format(confirmCloseMsg, "main"), message?.text)
     }
 
-    /**
-     * Used in tests to setup real (non mocked) DatabasesPresenter to test
-     * how does DatabasesPresenter and DatabasesFragment are integrated.
-     * @param[fragment] DatabasesFragment for which we will setup DatabasesPresenter.
-     */
-    private fun setupRealPresenter(fragment: DatabasesFragment) {
-        // list of databases for test
-        val databases = mutableListOf(
+    private fun setupPresenterAndAdapter() {
+        val presenter = DatabasesPresenter({ fragment }, mock(), app)
+        presenter.databases = mutableListOf(
             Database("main"), // locked
-            Database("test", "123") /* opened*/
+            Database("test", "123") // opened
         )
+        val adapter = DatabasesAdapter(presenter, context, app)
 
-        val presenter = DatabasesPresenter(fragment)
-        presenter.databases = databases
         fragment.presenter = presenter
+        fragment.adapter = adapter
     }
 
     /**
      * Used in tests to get measured and laid out recyclerview.
-     * @param[fragment] DatabasesFragment from which we will get the recyclerview.
      */
-    private fun getRecycler(fragment: DatabasesFragment): RecyclerView {
+    private fun getRecycler(): RecyclerView {
         // find recycler, measure and lay it out, so that later we can obtain its items
         val recycler: RecyclerView = fragment.view!!.findViewById(R.id.itemsList)
         recycler.measure(0, 0)
@@ -203,29 +188,25 @@ class DatabasesFragmentInst {
 
     @Test
     fun `when closing database lock icon should change`() {
-        scenario.onFragment {
-            setupRealPresenter(it)
-            it.presenter.closeDatabase(1)
-            val recycler = getRecycler(it)
+        setupPresenterAndAdapter()
+        fragment.presenter.closeDatabase(1)
+        val recycler = getRecycler()
 
-            // check that lock icon changed to locked
-            val itemLayout = recycler.getChildAt(1)
-            val lockImg = itemLayout?.findViewById<ImageView>(R.id.itemIcon)
-            assertEquals(R.drawable.ic_locked, lockImg?.tag)
-        }
+        // check that lock icon changed to locked
+        val itemLayout = recycler.getChildAt(1)
+        val lockImg = itemLayout.findViewById<ImageView>(R.id.itemIcon)
+        assertEquals(R.drawable.ic_locked, lockImg.tag)
     }
 
     @Test
     fun `when deleting database it should disappear from recycler`() {
-        scenario.onFragment {
-            setupRealPresenter(it)
-            it.presenter.deleteDatabase(0)
-            val recycler = getRecycler(it)
+        setupPresenterAndAdapter()
+        fragment.presenter.deleteDatabase(0)
+        val recycler = getRecycler()
 
-            // check that the first database is `test` as `main` was deleted
-            val itemLayout = recycler.getChildAt(0)
-            val databaseName = itemLayout?.findViewById<TextView>(R.id.itemName)
-            assertEquals("test", databaseName?.text)
-        }
+        // check that the first database is `test` as `main` was deleted
+        val itemLayout = recycler.getChildAt(0)
+        val databaseName = itemLayout.findViewById<TextView>(R.id.itemName)
+        assertEquals("test", databaseName.text)
     }
 }
