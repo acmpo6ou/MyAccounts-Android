68a65613c0d8105623cc8201741d552bf5a39881
Acmpo6ou <acmpo6ou@gmail.com>
2021-04-25 15:25:49 +0300

    Fix CreateEditDatabaseModelTests

--- app/src/test/java/com/acmpo6ou/myaccounts/create_edit_database/CreateEditDatabaseModelTests.kt
+++ app/src/test/java/com/acmpo6ou/myaccounts/create_edit_database/CreateEditDatabaseModelTests.kt
@@ -38,26 +38,21 @@ class CreateEditDatabaseModelTests : ModelTest() {
     @get:Rule
     val taskExecutorRule = InstantTaskExecutorRule()
 
-    private open class TestModel : CreateEditDatabaseModel() {
-        override suspend fun apply(name: String, password: String) {
-        }
-    }
-
-    private val model = TestModel()
-    private lateinit var spyModel: TestModel
+    private lateinit var model: TestDatabaseModel
+    private lateinit var spyModel: TestDatabaseModel
 
     private val name = faker.str()
     override val password = faker.str()
 
     @Before
     fun setup() {
-        model.app = MyApp()
-        model.app.databases = mutableListOf(
+        app = MyApp()
+        app.databases = mutableListOf(
             Database("main"),
             Database("test", "123")
         )
-        model.defaultDispatcher = Dispatchers.Unconfined
-        model.uiDispatcher = Dispatchers.Unconfined
+
+        model = TestDatabaseModel(app)
         spyModel = spy(model)
     }
 
@@ -71,14 +66,14 @@ class CreateEditDatabaseModelTests : ModelTest() {
     fun `validateName should set existsNameErr to true when Database with such name exists`() {
         // even if name contains unsupported characters
         model.validateName("m/a/i/n/") // will become `main` when cleaned by fixName
-        assertTrue(model.existsNameErr)
+        assertTrue(model.existsNameErr.value!!)
     }
 
     @Test
     fun `validateName should use fixName`() {
         val name = " \\/%$" // this name will be empty when cleaned by fixName
         model.validateName(name)
-        assertTrue(model.emptyNameErr)
+        assertTrue(model.emptyNameErr.value!!)
     }
 
     @Test
@@ -108,5 +103,11 @@ class CreateEditDatabaseModelTests : ModelTest() {
         runBlocking {
             verify(spyModel).apply(name, password)
         }
+    }
+}
+
+open class TestDatabaseModel(override val app: MyApp) : CreateEditDatabaseModel() {
+    override val uiDispatcher = Dispatchers.Unconfined
+    override suspend fun apply(name: String, password: String) {
     }
 }
