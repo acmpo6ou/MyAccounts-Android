f6cd953ec45f86a290f04b5ad20433c77868e83c
Acmpo6ou <acmpo6ou@gmail.com>
2021-04-27 15:56:52 +0300

    Adapt AttachedFilesAdapter to Hilt

--- app/src/main/java/com/acmpo6ou/myaccounts/account/create_edit_account/AttachedFilesAdapter.kt
+++ app/src/main/java/com/acmpo6ou/myaccounts/account/create_edit_account/AttachedFilesAdapter.kt
@@ -19,24 +19,30 @@
 
 package com.acmpo6ou.myaccounts.account.create_edit_account
 
+import android.content.Context
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.PopupMenu
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
 import com.acmpo6ou.myaccounts.R
+import dagger.hilt.android.qualifiers.ActivityContext
+import dagger.hilt.android.scopes.FragmentScoped
+import javax.inject.Inject
 
-class AttachedFilesAdapter(val view: CreateEditAccountFragment) :
-    RecyclerView.Adapter<AttachedFilesAdapter.ViewHolder>() {
+@FragmentScoped
+class AttachedFilesAdapter @Inject constructor(
+    private val view: CreateEditAccountFragment,
+    @ActivityContext private val context: Context,
+) : RecyclerView.Adapter<AttachedFilesAdapter.ViewHolder>() {
 
-    val viewModel get() = view.viewModel
-    private val attachedFiles get() = viewModel.attachedFilesList
+    private val attachedFiles get() = view.viewModel.attachedFilesList
+    override fun getItemCount() = attachedFiles.size
 
     val addedObserver = Observer<Int> {
         notifyItemChanged(it)
@@ -58,32 +64,29 @@ class AttachedFilesAdapter(val view: CreateEditAccountFragment) :
 
         // set popup menu on item
         holder.menu.setOnClickListener { it ->
-            val popup = PopupMenu(view.myContext, it)
+            val popup = PopupMenu(context, it)
             popup.inflate(R.menu.attached_file_item_menu)
 
             // set color of `Remove` item to red
             popup.menu.findItem(R.id.remove_attached_file).let {
                 val spanStr = SpannableString(it.title)
-                val redColor = ContextCompat.getColor(view.myContext, R.color.red)
+                val redColor = ContextCompat.getColor(context, R.color.red)
                 spanStr.setSpan(ForegroundColorSpan(redColor), 0, it.title.length, 0)
                 it.title = spanStr
             }
 
             popup.setOnMenuItemClickListener {
-                viewModel.removeFile(position)
+                view.viewModel.removeFile(position)
                 true
             }
             popup.show()
         }
     }
 
-    override fun getItemCount() = attachedFiles.size
-
     /**
-     * Represents ViewHolder for item of attached files list.
+     * ViewHolder for item of attached files list.
      */
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        var attachImage: ImageView = view.findViewById(R.id.itemIcon)
         var fileName: TextView = view.findViewById(R.id.itemName)
         var menu: TextView = view.findViewById(R.id.dots_menu)
     }
