d3d11d4dce9a5fe75fb80b528a2b1b0282e3f047
Acmpo6ou <acmpo6ou@gmail.com>
2021-08-27 18:31:41 +0300

    Implement LockActivity

--- app/src/main/java/com/acmpo6ou/myaccounts/LockActivity.kt
+++ app/src/main/java/com/acmpo6ou/myaccounts/LockActivity.kt
@@ -20,15 +20,71 @@
 package com.acmpo6ou.myaccounts
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class LockActivity : AppCompatActivity() {
+    @Inject
+    lateinit var app: MyApp
+
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
+
+    private val authCallback = object : BiometricPrompt.AuthenticationCallback() {
+        override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+            super.onAuthenticationSucceeded(result)
+            app.isLocked = false
+            this@LockActivity.unlock()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_MyAccounts_NoActionBar)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lock)
+
+        authenticateUser()
     }
 
     override fun onBackPressed() {
         // Do not allow going back from LockActivity
+    }
+
+    /**
+     * Unlocks the app by going back from LockActivity.
+     */
+    private fun unlock() {
+        super.onBackPressed()
+    }
+
+    /**
+     * Builds and displays biometric authentication dialog.
+     */
+    fun authenticateUser(view: View? = null) {
+        val executor = ContextCompat.getMainExecutor(this)
+        biometricPrompt = BiometricPrompt(this, executor, authCallback)
+
+        val APP_NAME = resources.getString(R.string.app_name)
+        val SUBTITLE = resources.getString(R.string.app_is_locked)
+        val APP_LOCK_TIP = resources.getString(R.string.app_lock_tip)
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle(APP_NAME)
+            .setSubtitle(SUBTITLE)
+            .setDescription(APP_LOCK_TIP)
+            .setAllowedAuthenticators(BIOMETRIC_STRONG or DEVICE_CREDENTIAL)
+            .build()
+
+        biometricPrompt.authenticate(promptInfo)
     }
 }
