8e598e21d893c796e460851f2cb370fd04dfac08
acmpo6ou <acmpo6ou@gmail.com>
2021-10-28 12:32:04 +0300

    Change test about loading default icon
    We should load default icon if there is no match because of how recycler view works

--- app/src/test/java/com/acmpo6ou/myaccounts/accounts_list/AccountsAdapterTests.kt
+++ app/src/test/java/com/acmpo6ou/myaccounts/accounts_list/AccountsAdapterTests.kt
@@ -22,10 +22,12 @@ package com.acmpo6ou.myaccounts.accounts_list
 import android.content.res.AssetManager
 import com.acmpo6ou.myaccounts.account.accounts_list.AccountsAdapter
 import com.caverock.androidsvg.SVGImageView
-import com.nhaarman.mockitokotlin2.*
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
 import org.junit.Before
 import org.junit.Test
-import org.mockito.ArgumentMatchers.anyString
 
 class AccountsAdapterTests {
     private lateinit var adapter: AccountsAdapter
@@ -66,9 +68,8 @@ class AccountsAdapterTests {
     }
 
     @Test
-    fun `loadAccountIcon should NOT call loadAccountIcon if there is no match`() {
-        // because we have a default icon
+    fun `loadAccountIcon should load default icon if there is no match`() {
         adapter.loadAccountIcon(mockImage, "there will be no match")
-        verify(mockImage, never()).setImageAsset(anyString())
+        verify(mockImage).setImageAsset("account.svg")
     }
 }
